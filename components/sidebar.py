b64fb2412d6d471811e128f73561d5d31848c76d
2bfree <2bfree@gmail.com>
2024-03-20 16:33:42 +0900

    main

--- components/sidebar.py
+++ components/sidebar.py
@@ -12,7 +12,7 @@ def sidebar():
     with st.sidebar:
         st.markdown(
             "## SFA KnowledgeGPT는\n"
-            "업로드한 문서에 있는 내용만 사용하여  "
+            "업로드한 문서에 있는 내용만 사용하여     "
             "질문에 대한 정확한 답변과 참조한 내용의 위치를 알려주는\n\n"
             "사내 GPT 서비스의 Prototype입니다."
         )
