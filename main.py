d02c0dd7fd0daf28c7f5393c7e0fa86901674711
2bfree <2bfree@gmail.com>
2024-03-20 15:36:02 +0900

    main

--- main.py
+++ main.py
@@ -40,15 +40,15 @@ openai_api_key = st.session_state.get("OPENAI_API_KEY")
 
 if not openai_api_key:
     st.warning(
-        "Enter your OpenAI API key in the sidebar. You can get a key at"
-        " https://platform.openai.com/account/api-keys."
+        "OpenAI API key를 사이드바에 입력하셔야. 시작됩니다. API key는"
+        " https://platform.openai.com/account/api-keys.에서 발급 받을 수 있습니다."
     )
 
 
 uploaded_file = st.file_uploader(
-    "Upload a pdf, docx, or txt file",
+    "검색할 파일을 업로드 하세요. (현재는 pdf, docx, txt만 가능합니다.)",
     type=["pdf", "docx", "txt"],
-    help="Scanned documents are not supported yet!",
+    help="스캔한 문서의 분석은 아직 못합니다.",
 )
 
 model: str = st.selectbox("Model", options=MODEL_LIST)  # type: ignore
