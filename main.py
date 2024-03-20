1901dab4e72d516938ae2cf5d5448d9fbe4a4a31
2bfree <2bfree@gmail.com>
2024-03-20 13:40:07 +0900

    main

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from core.utils import get_llm
 
 EMBEDDING = "openai"
 VECTOR_STORE = "faiss"
-MODEL_LIST = ["gpt-3.5-turbo", "gpt-4"]
+MODEL_LIST = ["gpt-3.5-turbo", "gpt-4", "GPT-4-Turbo"]
 
 # Uncomment to enable debug mode
 # MODEL_LIST.insert(0, "debug")
